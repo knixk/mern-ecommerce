a285e4813c6be5cee877188d17283ae19c7e3114
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-01-08 18:31:41 +0530

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import Product from "./components/Product";
 import { Container } from "react-bootstrap";
 
 function App() {
@@ -8,7 +10,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <HomeScreen></HomeScreen>
         </Container>
       </main> 
       <Footer/> 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,14 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png'
 
 const Header = () => {
   return <header>
     <Navbar bg='dark' variant="dark" expand='lg' collapseOnSelect>
         <Container> 
-            <Navbar.Brand href="/">ProShop</Navbar.Brand>
+            <Navbar.Brand href="/">
+                <img src={logo} alt="logo" />
+                ProShop</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
             <Navbar.Collapse>
                 <Nav className="ms-auto">
