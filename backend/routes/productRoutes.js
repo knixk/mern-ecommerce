7b82d2c1015520e71fce65a03a3ca880dac21f49
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-01-09 21:57:54 +0530

    fixed api inconsistencies

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,18 +1,29 @@
 import express from "express";
-
 const router = express.Router();
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
 
-import products from "../data/products.js";
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    // we are getting the id from the URL params
+    // const product = products.find((p) => p._id === req.params.id);
+    const product = await Product.findById(req.params.id);
 
-router.get("/", (req, res) => {
-  res.json(products);
-});
+    if (product) {
+      return res.json(product);
+    }
 
-router.get("/:id", (req, res) => {
-  // we are getting the id from the URL params
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+    res.status(404).json({ message: "Product not found!" });
+  })
+);
 
 export default router;
