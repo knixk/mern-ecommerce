15334d35d4d1b2aec28403f5943ce69d621415e7
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-01-09 22:29:50 +0530

    routes and controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,29 +1,11 @@
 import express from "express";
 const router = express.Router();
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/productController,js";
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    // we are getting the id from the URL params
-    // const product = products.find((p) => p._id === req.params.id);
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      return res.json(product);
-    }
-
-    res.status(404).json({ message: "Product not found!" });
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 dotenv.config();
 
@@ -10,11 +11,17 @@ connectDB(); // connect to mongoDB
 
 const app = express();
 
+// routes
 app.get("/", (req, res) => {
   res.send("Api is running");
 });
 
 app.use("/api/products", productRoutes);
+
+// err check middleware
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
