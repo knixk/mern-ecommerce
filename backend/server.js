9a40f237660e8e49248982cefe82d53b046ad1f5
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-01-09 14:59:35 +0530

    async Handler

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 dotenv.config();
 
 const port = process.env.PORT || 8000;
@@ -14,15 +14,7 @@ app.get("/", (req, res) => {
   res.send("Api is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/product/:id", (req, res) => {
-  // we are getting the id from the URL params
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
