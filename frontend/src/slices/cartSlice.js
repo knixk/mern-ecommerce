55f11181b584c98a79a705dfa241762cfa823f88
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-01-10 12:44:39 +0530

    cartSlice

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -9,7 +9,16 @@ const initialState = localStorage.getItem("cart")
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map
+      }
+    },
+  },
 });
 
 export default cartSlice.reducer;
