7cac3b4646b98ba3c0bc3ea4f588495e4bc8e9dc
Kanishk Shrivastava <shrivastavakanishk3@gmail.com>
2024-01-10 12:35:11 +0530

    reducers and slices

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,31 +1,46 @@
-import { useEffect, useState } from "react";
-import { Row, Col } from "react-bootstrap";
+// import { useEffect, useState } from "react";
 import Product from "../components/Product";
-import axios from "axios"
+import { Row, Col } from "react-bootstrap";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+// import axios from "axios"
 // import products from "../products";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);  
-    };
+  // const [products, setProducts] = useState([]);
 
-    fetchProducts();
-  }, []);
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     const { data } = await axios.get('/api/products');
+  //     setProducts(data);
+  //   };
+
+  //   fetchProducts();
+  // }, []);
 
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product}></Product>
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <Loader></Loader>
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
